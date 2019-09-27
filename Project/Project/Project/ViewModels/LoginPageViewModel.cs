5b79a9590dc318d08d5db903a8168c4ebc0b76c7
LightspeedDude <43783680+LightspeedDude@users.noreply.github.com>
2019-09-27 11:36:42 -0400

    Add files via upload

---
+++ Project/Project/Project/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Project.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Project.ViewModels
+{
+    public class LoginPageViewModel: BaseViewModel
+    {
+        public LoginPageViewModel()
+        {
+            PageName = "Login Page";
+        }
+    }
+}
---
+++ Project/Project/Project/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Project.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Project.ViewModels
+{
+    public class SearchPageViewModel : BaseViewModel
+    {
+            public SearchPageViewModel()
+            {
+                PageName = "Search Page";
+            }
+        
+    }
+}
