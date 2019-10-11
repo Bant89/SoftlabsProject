1d254ce407b690783c3a2af6399ac68f34ade2fc
Emilio Escaño <emiliojoseescano@hotmail.com>
2019-10-11 14:54:28 -0400

    Improvemetns

--- Project/Project/Project.Android/MainActivity.cs
+++ Project/Project/Project.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Prism.Ioc;
 
 namespace Project.Droid
 {
-    [Activity(Label = "Project", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SoftLabs", Icon = "@drawable/jellylogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
